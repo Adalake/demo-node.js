641ceaff661d7d75748629a8431f515e77040aad
Adalake <gaomintv@sohu.com>
2021-07-27 22:32:39 +0800

    http+fs

--- articleGet/index3.js
+++ articleGet/index3.js
@@ -1,25 +1,20 @@
-// 实现：利用http module来展示html文件（使用fs和http module） 待续。。。
+// 实现：利用http module来展示html文件（使用fs和http module
+// server端在接到http请求后，如何把用户所需的html文件展示出来：
 var http = require("http");
-var express = require("express");
-var app = express();
-app.get("/", function (req, res) {
-  var url = "http://www.baidu.com";
-  http.get(url, function (response) {
-    var html = "";
-    response.on("data", function (data) {
-      html += data;
-    });
-    response.on("end", function () {
-      // res.render("test", {
-      //   html: html,
-      // });
-      res.send(html);
-    });
-  });
-});
+var fs = require("fs"); //fs会帮助你访问你的file system
 
-var server = app.listen(8082, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log("应用实例，访问地址为 http://%s:%s", host, port);
-});
+function onRequest(req, res) {
+  res.writeHead(200, { "Content-Type": "text/html" }); //这里的类型要用html，使用plain会展示整个html文件。
+  fs.readFile("./index3.html", null, function (error, data) {
+    if (error) {
+      res.writeHead(404);
+      res.write("File not found!");
+    } else {
+      //fs正常打开文件，并将index3.html写入response中
+      res.write(data);
+    }
+    res.end();
+  });
+}
+
+http.createServer(onRequest).listen(8084);
